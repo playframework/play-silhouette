200d738ec2eb9093fe9b746f591c2e6ea11edba4
Simon Alexander Ramzi <simon.ramzi@heyjournal.com>
2022-04-08 17:24:57 +0100

    Updating to 7.0.7-SNAPSHOT.

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -57,7 +57,7 @@ object BasicSettings extends AutoPlugin {
 
   override def projectSettings = Seq(
     organization := "io.github.honeycomb-cheesecake",
-    version := "7.0.6",
+    version := "7.0.7-SNAPSHOT",
     resolvers ++= Dependencies.resolvers,
     scalaVersion := "2.13.1",
     crossScalaVersions := Seq("2.13.1", "2.12.10"),
