a89dfcaa8f31e16a535a08c63cb127a8672fd254
Scala Steward <scala_steward@virtuslab.com>
2025-03-31 19:24:34 +0000

    [9.x] specs2-core, specs2-matcher-extra 4.21.0 (was 4.20.9)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
     }
 
     object Specs2 {
-      private val version = "4.20.9"
+      private val version = "4.21.0"
       val core = "org.specs2" %% "specs2-core" % version
       val matcherExtra = "org.specs2" %% "specs2-matcher-extra" % version
     }
