47123ee6863f2fc22cb621fafb26fb548f1db3cc
Scala Steward <scala_steward@virtuslab.com>
2025-12-12 21:04:42 +0000

    izumi-reflect 3.0.9 (was 3.0.8)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,6 +47,6 @@ object Dependencies {
     val casClientSupportSAML = "org.apereo.cas.client" % "cas-client-support-saml" % "4.0.4"
     val apacheCommonLang = "org.apache.commons" % "commons-lang3" % "3.20.0"
     val googleAuth = "com.warrenstrange" % "googleauth" % "1.5.0"
-    val izumiReflect = "dev.zio" %% "izumi-reflect" % "3.0.8" // Scala 3 replacement for scala 2 reflect universe
+    val izumiReflect = "dev.zio" %% "izumi-reflect" % "3.0.9" // Scala 3 replacement for scala 2 reflect universe
   }
 }
